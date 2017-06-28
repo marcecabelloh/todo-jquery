a2795f74c563c17e6253aa4e32680e5b5e7fbbe6
marcecabelloherrera <marcecabello.h@gmail.com>
2017-06-28 14:08:17 -0400

    jquery remover elementos

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,19 @@
 //funcion jquery que ingrese tareas ordenadas asignada por el usuario
+
 $(document).ready(
   function(){
-    $('#button').click(
+    $('#button').click(//al hacer click en el boton se ejecute la funcion
       function(){
-        var input = $('input[name=ListItem]').val();
-        $('ol').append('<li>' + input + '</li>');
+        var input = $('input[name=ListItem]').val();//valor de nuestro input 
+        $('ol').append('<p><input class="with-gap" name="group3" type="radio" id="test5" checked /><label for="test5">' + input +'</label></p>' +'<a class="waves-effect waves-light btn" id="remover">remove</a>');//para que lista quede dentro de un li
       }); 
+
+//busco remover las tareas cuando le de click al boton remover
+    $('remover').click(
+        function(){
+
+        }
+      )
   }
   );
+
