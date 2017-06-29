c992e8f07565346d0115fa0417225d4291d9dc83
marcecabelloherrera <marcecabello.h@gmail.com>
2017-06-29 10:31:35 -0400

    botón logra borrar

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,24 +7,22 @@ $(document).ready(function(){
     $('#button').click( function(e){
         e.preventDefault();
         var input = $('input[name=ListItem]').val();//valor de nuestro input 
-        $('ol').append('<p><input class="with-gap" name="group3" type="radio" id="test5" checked /><label for="test5">' + input +'</label> <a class="waves-effect waves-light btn">Borrar</a></p>');//para que lista quede dentro de un li
+        $('ol').append('<li><input type="checkbox"/>' + input + '<a class="waves-effect waves-light btn delete" id="delete">Borrar</a></li>');//para que lista quede dentro de un li
           $('#todo-list-item').val("");
 
       }); 
 
-//intento que tareas pasen a mi div si presiono el check-box
-$('#test5').click(//al hacer click en el boton se ejecute la funcion
+/*$('#test5').click(//al hacer click en el boton se ejecute la funcion
       function(){
-        var nuevoDiv = $('nuevo-div').append('p');
-      }); 
+      var nuevoDiv = $('nuevo-div').append('p');
+      }); */
 
 //busco remover las tareas cuando le de click al boton remover
-$('.btn').click(function(r){
-  $(r).remove();
-  localStorage.setItem('listItems', $('#list-items').html());
-       
+$(document).on('click' , '.delete' , function(){
+ $(this).parent().remove();
+ //localStorage.setItem('listItems', $('#list-items').html());
 
-});
+  });
 });
 
 
