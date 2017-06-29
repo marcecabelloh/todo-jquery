d9bd3bd1dfe2492066801d889da500db66b8da4a
marcecabelloherrera <marcecabello.h@gmail.com>
2017-06-28 22:30:47 -0400

    jquery agregar tareas removerlas

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,30 @@
 //funcion jquery que ingrese tareas ordenadas asignada por el usuario
 
-$(document).ready(
-  function(){
-    $('#button').click(//al hacer click en el boton se ejecute la funcion
-      function(){
+$(document).ready(function(){
+
+   $('#list-items').html(localStorage.getItem('listItems'));
+
+    $('#button').click( function(e){
+        e.preventDefault();
         var input = $('input[name=ListItem]').val();//valor de nuestro input 
-        $('ol').append('<p><input class="with-gap" name="group3" type="radio" id="test5" checked /><label for="test5">' + input +'</label></p>' +'<a class="waves-effect waves-light btn" id="remover">remove</a>');//para que lista quede dentro de un li
+        $('ol').append('<p><input class="with-gap" name="group3" type="radio" id="test5" checked /><label for="test5">' + input +'</label> <a class="waves-effect waves-light btn">Borrar</a></p>');//para que lista quede dentro de un li
+          $('#todo-list-item').val("");
+
+      }); 
+
+//intento que tareas pasen a mi div si presiono el check-box
+$('#test5').click(//al hacer click en el boton se ejecute la funcion
+      function(){
+        var nuevoDiv = $('nuevo-div').append('p');
       }); 
 
 //busco remover las tareas cuando le de click al boton remover
-    $('remover').click(
-        function(){
+$('.btn').click(function(r){
+  $(r).remove();
+  localStorage.setItem('listItems', $('#list-items').html());
+       
 
-        }
-      )
-  }
-  );
+});
+});
+
 
