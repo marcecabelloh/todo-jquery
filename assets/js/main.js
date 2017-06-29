34bfdde71e6267ab66f665aa28eb5ee32821aae4
marcecabelloherrera <marcecabello.h@gmail.com>
2017-06-29 11:17:09 -0400

    visibilidad de checkbox y mejora de diseño

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,5 @@
+var i = 0; //contador, para poder introducir los check como tareas creadas y completas
+
 //funcion jquery que ingrese tareas ordenadas asignada por el usuario
 
 $(document).ready(function(){
@@ -8,21 +10,25 @@ $(document).ready(function(){
         e.preventDefault();
         var input = $('input[name=ListItem]').val();//valor de nuestro input 
         $('ol').append('<li><input type="checkbox"/>' + input + '<a class="waves-effect waves-light btn delete" id="delete">Borrar</a></li>');//para que lista quede dentro de un li
-          $('#todo-list-item').val("");
+          $('#todo-list-item').val("");//para que mi input quede limpio
 
       }); 
 
-/*$('#test5').click(//al hacer click en el boton se ejecute la funcion
-      function(){
-      var nuevoDiv = $('nuevo-div').append('p');
-      }); */
 
 //busco remover las tareas cuando le de click al boton remover
 $(document).on('click' , '.delete' , function(){
  $(this).parent().remove();
- //localStorage.setItem('listItems', $('#list-items').html());
 
   });
+
+/*intento agregar tareas a tareas completadas*/
+/*function onSelectItem(item) {
+    var html = '<div class="row item" id="' + item.id + '">' + $(item).html() + '</div>'; //id item nuevo
+    console.log(html);
+    $(item).remove();
+    $(".completed-items").append(html); //añadir completed items al html
+}
 });
+
 
 
